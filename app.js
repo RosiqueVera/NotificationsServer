c10874ab90276ba11c758c6d13bb2ef4977a703b
OARosique <oarosique@interceramic.com>
2023-09-28 16:06:32 -0600

    documentando api

--- app.js
+++ app.js
@@ -5,23 +5,31 @@
  * Servidor de maquetación de notificaciones en firebase con Aut0
  */
 
-//Imports
-const appAdmin = require('firebase-admin/app');
+//Imports referentes a express
 const express = require('express');
-
 const requestRoutes = require("./routes/notifications");
+const path = require("path");
 
+//Swagger
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerSpec = {
+    definition: {
+        openapi: "3.1.0",
+        info: {
+            title: "Servidor de notificaciones con NodeJS",
+            version: "1.0"
+        },
+        servers: [
+            {
+                url: "http://localhost:3000"
+            }
+        ],
 
-const corpo8 = require('./keys/corpo-8-firebase-adminsdk-3idy4-67e87f7f17.json');
+    },
+    apis: [`${path.join(__dirname, "./routes/*.js")}`]
+}
 
-//Inicializamos la aplicación de firebase
-var defaultApp = appAdmin.initializeApp({
-    credential: appAdmin.cert(corpo8),
-    projectId: 'corpo-8'
-});
-/* var otherApp = appAdmin.initializeApp(pushDemo,'PushDemo');
-
-console.log(otherApp.name); */
 
 //Declaración del uso de express (Para iniciar el servidor web)
 const app = express();
@@ -30,18 +38,22 @@ const app = express();
 //Configuación del uso de json en express
 app.use(express.json());
 //esqueleto de la ruta
-app.use('/notificationServer/api/v1',requestRoutes);
+app.use('/notificationServer/api/v1/firebase', requestRoutes);
+//para visualización de la documentación
+app.use(
+    '/api-doc', 
+    swaggerUI.serve,
+    swaggerUI.setup(swaggerJsDoc(swaggerSpec))
+);
 
 //default
 app.get("/", (_, res) => {
     res.send("Bienvenido al servidor creado con NodeJS");
-  });
+});
 app.get("/notificationServer/api/v1", (_, res) => {
     res.send("REST Api dedicada al manejo de notificaciones con el uso de firebase y la nueva API de Cloud Messaging");
-  });
+});
 //Configuración del puerto del servidor
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Server stated on port 3000");
 });
-
-
--- routes/notifications.js
+++ routes/notifications.js
@@ -1,8 +1,74 @@
-const express = require("express");
+const appAdmin = require('firebase-admin/app');
 const messaging = require('firebase-admin/messaging');
+const express = require("express");
 
 const router = express.Router();
 
+const corpo8 = require('../keys/corpo-8-firebase-adminsdk-3idy4-67e87f7f17.json');
+const pushDemo = require('../keys/flutterpushnotifications-bfb3e-firebase-adminsdk-3txzv-61dd017d09.json');
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      firebase:
+ *          type: object
+ *          properties:
+ *              token:
+ *                  type: string
+ *                  description: token del dispositivo
+ *              information:
+ *                  type: string
+ *                  description: JSON de información que llevará el payload de la notificación
+ *              badgecount:
+ *                  type: integer
+ *                  descripction: Número de notificaciones pendientes
+ *          required:
+ *              -token
+ *              -badgecount
+ *          example:
+ *              token: eZRypq5KRh-IgoOOjjd8ow:APA91bEl_InBad4orc4WbjlqwacxEQUxAVsXvK7FP-LsDh0kRaIiw0ou5f6vTs6k-oWWJCJJpv0LhxljoLHzTrv6bJjJ9Rllvmj1I40zQe80K7kzx1d4hVAGi1w7k3ulK_TCLuWeUWSv
+ *              information: { "dato1": "1", "dato2":"dos" }
+ *              badgecount: 10
+ */
+
+
+//Inicializamos la aplicación de firebase
+var defaultApp = appAdmin.initializeApp({
+    credential: appAdmin.cert(corpo8),
+    projectId: 'corpo-8'
+});
+var otherApp = appAdmin.initializeApp(appAdmin.cert(pushDemo),'PushDemo');
+
+/**
+ * @swagger
+ * /notificationServer/api/v1/firebase/onlyDevice:
+ *  post:
+ *      summary: envia la notificación a un único dispositivo en la aplicación default a través del token 
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      $ref: '#/components/schemas/firebase'
+ *      responses:
+ *          200:
+ *              description: La notificación se envió correctamente
+ *              content:
+ *                  applicattion/json:
+ *                      schema:
+ *                          type: object
+ *                          $ref: '#/components/schemas/successfull'
+ *          400:
+ *              description: No se envió la notificación
+ *              content:
+ *                  applicattion/json:
+ *                      schema:
+ *                          type: object
+ *                          $ref: '#/components/schemas/failure'
+ *          500:
+ *              description: Ocurrió un error en el servidor
+ */
 router.post("/onlyDevice", function(req, res){
     //console.info(req.body);
     //Esta variable obtiene de la petición el token al cual se le mandará la información
@@ -35,6 +101,85 @@ router.post("/onlyDevice", function(req, res){
 
 });
 
+router.post("/project/:project", function(req, res){
+    
+    //Esta variable obtiene de la petición el token al cual se le mandará la información
+    const receivedToken = req.body.token;
+    const information = req.body.information;
+    const badgecount = req.body.badgecount;
+
+    const project = req.params.project;
+    var application = defaultApp.name;
+    if (project ==='pushDemo') {
+        application = otherApp.name;
+    }
+    const message = {
+        token: receivedToken,
+        notification: {
+            title: "Prueba",
+            body: "Notificación desde servidor de pruebas"
+        },
+        data:information,
+        android:{
+            notification:{
+                notification_count: badgecount ?? 1,
+                sound : "default"
+            }
+        },
+        apns:{
+            payload:{
+                aps:{
+                    notification_count: badgecount ?? 1,
+                    sound : "default"
+                }
+            }
+        }
+    };
+    messaging.getMessaging(appAdmin.getApp(application)).send(message)
+        .then((response) => {
+            res.status(200).json({
+                message: "Successfully send message",
+                messageId: response,
+            });
+            console.log("Successfully sent message: ", response);
+        })
+        .catch((error) => {
+            res.status(400);
+            res.send(error);
+            console.log("Error sending message", error);
+        });
+
+});
+/**
+ * @swagger
+ * /notificationServer/api/v1/firebase/multi_devices:
+ *  post:
+ *      summary: envia la notificación a los dispositivos especificados 
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      $ref: '#/components/schemas/firebase'
+ *      responses:
+ *          200:
+ *              description: La notificación se envió correctamente
+ *              content:
+ *                  applicattion/json:
+ *                      schema:
+ *                          type: object
+ *                          $ref: '#/components/schemas/successfull'
+ *          400:
+ *              description: No se envió la notificación
+ *              content:
+ *                  applicattion/json:
+ *                      schema:
+ *                          type: object
+ *                          $ref: '#/components/schemas/failure'
+ *          500:
+ *              description: Ocurrió un error en el servidor
+ */
 router.post("/multi_devices",function(req,res){
     //Obtenemos los tokens de los dispositivos a los que se enviará
     const tokens = req.body.tokens;
@@ -94,7 +239,36 @@ router.post("/multi_devices",function(req,res){
 },);
 
 
-
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      successfull:
+ *          type: object
+ *          properties:
+ *              message:
+ *                  type: string
+ *                  description: Mensaje de envio exitoso
+ *              messageId:
+ *                  type: string
+ *                  description: Aplicación/Id del mensaje en firebase
+ *          example:
+ *              message: Successfully send message
+ *              messageId: projects/corpo-8/messages/0:1695934698497806%73f1ea1973f1ea19
+ *      failure:
+ *          type: object
+ *          properties:
+ *              code:
+ *                  type: string
+ *                  description: tipo de error cometido
+ *              message:
+ *                  type: string
+ *                  descripcion: descripción del error
+ *          example:
+ *              code: "app/invalid-credential"
+ *              message: "Credential implementation provided to initializeApp() via the \"credential\" property failed to fetch a valid Google OAuth2 access token with the following error: \"Error fetching access token: Error while making request: getaddrinfo ENOTFOUND metadata.google.internal. Error code: ENOTFOUND\"."
+ * 
+ */
 function sendRequestToFCM(message, res) {
     messaging.getMessaging().send(message)
         .then((response) => {
